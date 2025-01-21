f2d3391aaf3489203155bfa7f2969171b07dc56d
anson7ahh <nguyenanson554@gmail.com>
2025-01-21 10:45:03 +0700

    feat:create ui profile

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 module.exports = app;
